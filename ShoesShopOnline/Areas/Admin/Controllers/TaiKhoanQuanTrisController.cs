4c768fe77295dc000b18593de2b0e979c9f9b9f8
QuachThao2010 <quachthao2010@gmail.com>
2022-01-03 23:06:14 +0700

    Thao update ban cuoi cung de Tam fix

---
+++ ShoesShopOnline/Areas/Admin/Controllers/TaiKhoanQuanTrisController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using ShoesShopOnline.Models;
+
+namespace ShoesShopOnline.Areas.Admin.Controllers
+{
+    public class TaiKhoanQuanTrisController : BaseController
+    {
+        private Shoes db = new Shoes();
+
+        // GET: Admin/TaiKhoanQuanTris
+        public ActionResult Index(int? page)
+        {
+            ViewBag.Error = TempData["Error"];
+            ViewBag.Success = TempData["Success"];
+            var taikhoans = db.TaiKhoanQuanTris.Select(tk => tk);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(taikhoans.OrderBy(tk => tk.MaTK).ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Admin/TaiKhoanQuanTris/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoanQuanTri taiKhoanQuanTri = db.TaiKhoanQuanTris.Find(id);
+            if (taiKhoanQuanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoanQuanTri);
+        }
+
+        // GET: Admin/TaiKhoanQuanTris/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/TaiKhoanQuanTris/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTK,TenDangNhap,MatKhau,HoTenUser,LoaiTK,TrangThai")] TaiKhoanQuanTri taiKhoanQuanTri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaiKhoanQuanTris.Add(taiKhoanQuanTri);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taiKhoanQuanTri);
+        }
+
+        // GET: Admin/TaiKhoanQuanTris/Edit/5
+        public ActionResult Edit(int? id)
+        {        
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoanQuanTri taiKhoanQuanTri = db.TaiKhoanQuanTris.Find(id);
+            if (taiKhoanQuanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoanQuanTri);
+        }
+
+        // POST: Admin/TaiKhoanQuanTris/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, bool TrangThai)
+        {
+            string Error = null;
+            string Success = null;
+            try
+            {
+                TaiKhoanQuanTri login = (TaiKhoanQuanTri)Session[ShoesShopOnline.Session.ConstaintUser.ADMIN_SESSION];
+                if (ModelState.IsValid)
+                {
+                    TaiKhoanQuanTri acc = (from tk in db.TaiKhoanQuanTris where tk.MaTK.Equals(id) select tk).FirstOrDefault();
+                    if (login.MaTK == acc.MaTK)
+                    {
+                        Error = "Bạn không thể sửa tài khoản này!";
+                    }
+                    else
+                    {
+                        var taiKhoanQuanTri = db.TaiKhoanQuanTris.Find(id);
+                        taiKhoanQuanTri.TrangThai = TrangThai;
+                        db.Entry(taiKhoanQuanTri).State = EntityState.Modified;
+                        db.SaveChanges();
+                        Success = "Update thành công!";
+                    }
+                }
+                TempData["Error"] = Error;
+                TempData["Success"] = Success;
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi edit dữ liệu! " + ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Admin/TaiKhoanQuanTris/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoanQuanTri taiKhoanQuanTri = db.TaiKhoanQuanTris.Find(id);
+            if (taiKhoanQuanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoanQuanTri);
+        }
+
+        // POST: Admin/TaiKhoanQuanTris/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaiKhoanQuanTri taiKhoanQuanTri = db.TaiKhoanQuanTris.Find(id);
+            db.TaiKhoanQuanTris.Remove(taiKhoanQuanTri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ShoesShopOnline/Models/TaiKhoanNguoiDung.cs
@@ -0,0 +1,58 @@
+﻿namespace ShoesShopOnline.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TaiKhoanNguoiDung")]
+    public partial class TaiKhoanNguoiDung
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TaiKhoanNguoiDung()
+        {
+            HoaDons = new HashSet<HoaDon>();
+        }
+
+        [Key]
+        [DisplayName("Mã tài khoản")]
+        public int MaTK { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Tên đăng nhập")]
+        public string TenDangNhap { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [DisplayName("Mật khẩu")]
+        public string MatKhau { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [DisplayName("Họ tên")]
+        public string HoTen { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [DisplayName("Số điện thoại")]
+        public string SDT { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [DisplayName("Địa chỉ")]
+        public string DiaChi { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Trạng thái")]
+        public bool TrangThai { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<HoaDon> HoaDons { get; set; }
+    }
+}
